e9c4ad96f3c7f4da3f01443ee508e65a82ffd2b9
apst2020-01 <apst2020.01@gmail.com>
2021-01-04 21:40:35 +0000

    last please to commit to GITHUB

--- Davids/src/main/java/application/Shift.java
+++ Davids/src/main/java/application/Shift.java
@@ -2,6 +2,6 @@ package application;
 public class Shift {
 
 	public static void main(String[] args) {
-		 System.out.println( "Hello World sending back to GIT" );
+		 System.out.println( "Hello World sending back to GIT to commit" );
 	}
 }
