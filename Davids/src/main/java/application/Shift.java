2045a1b1bbf7c77aa2a79054ecb3c31d7f518bcd
apst2020-01 <apst2020.01@gmail.com>
2021-01-04 21:37:34 +0000

    hello world sending back to GIT

--- Davids/src/main/java/application/Shift.java
+++ Davids/src/main/java/application/Shift.java
@@ -2,6 +2,6 @@ package application;
 public class Shift {
 
 	public static void main(String[] args) {
-		 System.out.println( "Hello World!" );
+		 System.out.println( "Hello World sending back to GIT" );
 	}
 }
